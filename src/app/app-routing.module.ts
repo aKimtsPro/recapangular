dbfa85f8e78017411ebfe896c2278910ac3e2092
Aude Beurive <aude.beurive@bstorm.be>
2023-07-31 09:26:14 +0200

    mise en place projet, navbar, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasesComponent } from './bases/bases.component';
+import { ServicesComponent } from './services/services.component';
+import { ObservablesComponent } from './observables/observables.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'bases', component : BasesComponent },
+  { path : 'services', component : ServicesComponent },
+  { path : 'observables', component : ObservablesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BasesComponent } from './bases/bases.component';
+import { ServicesComponent } from './services/services.component';
+import { ObservablesComponent } from './observables/observables.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BasesComponent,
+    ServicesComponent,
+    ObservablesComponent
   ],
   imports: [
     BrowserModule,
